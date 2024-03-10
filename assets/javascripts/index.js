dd4685ec150ee834a8dd8429c56544c32e244fa0
nathanamackenzie <nathanamackenzie@gmail.com>
2024-03-10 14:59:14 -0400

    Creating API POST JS function

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,3 +1,5 @@
+var sessionID = Date.now()
+
 $( document ).ready(function() {
 	//Init waterpipe
 	var smokyBG = $('#wavybg-wrapper').waterpipe({
@@ -18,6 +20,44 @@ $( document ).ready(function() {
     });
 	$(".home").fadeIn(2000);
 
+    sessionID = Date.now()
+     
+   
 });
 
+console.log(getNow())
 
+function test(blah){
+    console.log(sessionID)
+    
+    fetch("https://ys8no68323.execute-api.us-east-1.amazonaws.com/test/visit",{
+        method: "POST",
+        body: JSON.stringify({
+            "TableName": "test",
+            "Item": {
+    	        "sessionID": {
+        	        "S":"1"
+                },
+                "epoch":{
+        	        "N": Date.now().toString()
+                },
+                "action":{
+        	        "S": blah
+                }
+            }
+        })
+    }).catch(error => { console.log(error); });
+}
+
+function getNow(){
+
+    var currentdate = new Date(); 
+    var datetime = (currentdate.getMonth()+1) + "/"
+                + currentdate.getDate()  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+    return datetime;
+}
